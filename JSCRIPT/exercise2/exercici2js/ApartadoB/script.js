e776be2d190fed6b5a49206c7ad90633ba2bbeff
SebastianLorenzano <selorenzano3@gmail.com>
2024-02-13 20:40:30 +0100

    exercise23css & exercise2js

--- JSCRIPT/exercise2/exercici2js/ApartadoB/script.js
+++ JSCRIPT/exercise2/exercici2js/ApartadoB/script.js
@@ -1,7 +1,8 @@
 let num1 = 14
 let num2 = 4
 let sum = num1 + num2
-
-const PI = 3.14159
-    document.write("Me llamo <b>" + name + "</b>, tengo <b>" + age + "</b> años y me conformo con un <b>" + grade + "</b> en JavaScript <br>")
-    document.write("El número PI es igual a <u>" + PI + "</u>")
+let res = num1 - num2;
+let product = num1 * num2;
+document.write('Primer número: <u>' + num1 + '</u><br>Segundo número: <u>' + num2 + '</u><br><br>Suma de ambos: <u>' + sum + '</u><br>Resta de ambos: <u>' + res + '</u><br>Producto de ambos: <u>' + product + '</u><br><br>Incrementamos el primer número. Ahora vale: <u>' + ++num1 + '</u><br>Decremenetamos el segundo número. Ahora vale: <u>' + --num2 + '</u>');
+let division = num1 / num2;
+document.write('<br><br>Division de ambos: <u>' + division + '</u>');
