30e32a253330dec7725f3d5bb0b4b80588aa4359
SebastianLorenzano <selorenzano3@gmail.com>
2024-02-29 19:47:51 +0100

    created Exercise4js

---
+++ JSCRIPT/exercise4/exercici4jsSebastianLorenzano/Apartado A/script.js
@@ -0,0 +1,13 @@
+document.write("Bucle hecho con <b>WHILE</b><br>")
+let i = 0
+while (i < 10) {
+    document.write("Que llegue ya <u>Semana Santa,</u> ¡por favor!<br>");
+    i++;
+}
+document.write("¿Ha quedado claro?<br><br>")
+document.write("Bucle hecho con <b>FOR</b><br>")
+for (let i = 0; i < 10; i++) {
+    document.write("Que llegue ya <u>Semana Santa,</u> ¡por favor!<br>")
+}
+document.write("¿Ha quedado claro?")
+
